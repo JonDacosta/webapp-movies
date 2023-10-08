29dff210f2771e8153639cba378a3069073fecfb
Jonatan Dacosta <dacostaafonso@gmail.com>
2023-10-09 00:21:42 +0200

    fix - test command

---
+++ app/services/api.tsx
@@ -0,0 +1,7 @@
+export const options = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIwMTBjOWYyMzI0M2M5NTkxNjVmZDk3NTNkOGRmZjUzZSIsInN1YiI6IjY1MTRhNzNjYmRkNTY4MDBlNDZiNjhjNiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.AxOgaOfeonCf3a4GT1ahG0LLFReyM9HMF8namSMD74k'
+    },
+};
